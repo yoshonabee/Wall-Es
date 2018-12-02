ccf3585a5acab2b6ecabb7ab4b779c5831f9b484
tigercosmos <phy.tiger@gmail.com>
2018-12-02 23:08:55 +0800

    main: newGame api

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,16 @@
 from flask import (
     Flask,
+    request,
     render_template,
     send_from_directory
 )
 
+from lib.game.game import Game
+
 # Create the application instance
 app = Flask(__name__, template_folder="../frontend")
+
+game = None
 
 # Create a URL route in our application for "/"
 @app.route('/')
@@ -22,6 +27,19 @@ def home():
 @app.route('/<path:path>')
 def send_js(path):
     return send_from_directory('../frontend', path)
+
+@app.route('/newGame')
+def newGame():
+    global game
+
+    height = int(request.args.get("height"))
+    width = int(request.args.get("width"))
+    t = (height + width)/2
+    game = Game(height, width)
+    game.setRandomMap(int(t*0.3), int(t*0.4)**2, int(t*0.3)**2)
+    game.printGodMap()
+    game.printConsoleMap()
+    return game.jsonMap()
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
