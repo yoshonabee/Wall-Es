827d457473c68a8a3e5b82808f199643fbd7aa17
B05611003 <B05611003@ntu.edu.tw>
2018-12-14 14:55:12 +0800

    improve

--- backend/test/test_game_alg.py
+++ backend/test/test_game_alg.py
@@ -5,49 +5,10 @@ import numpy as np
 
 from lib.game.game import Game, Command
 from lib.game.agent import Agent
-from lib.game.maps import ConsoleMap
 
-
-def testManualGameImageOutput1():
-    print("\n== init manual setting game ==")
-    height = 10
-    width = 10
-    game = Game(height, width)
-
-    obstacles = [
-        {"x": 1, "y": 1}, {"x": 2, "y": 2}, {"x": 3, "y": 3}
-    ]
-    targets = [
-        {"x": 4, "y": 4}, {"x": 5, "y": 5}, {"x": 6, "y": 6}
-    ]
-    game.setObstacles(obstacles)
-    game.setTargets(targets)
-    #game.setScore(100, 10, -0.01, -100)
-
-    game.printGodMap()
-
-    agents = {
-        0: Agent(0, 7, 6, height, width, r=3), # id, x, y, height, width
-    }
-    game.setAgents(agents)
-
-    game.printConsoleMap()
-
-    print("\n== 1st round ==")
-    game.runOneRound([Command(0, -1, -1)]) # (6, 5)
-    game.printConsoleMap()
-
-    print("\n== 2st round ==")
-    game.runOneRound([Command(0, -1, 0)]) # (5, 5)
-    game.printConsoleMap()
-
-    print("\n== 3st round ==")
-    game.runOneRound([Command(0, -1, 0)]) #(4, 5)
-    game.printConsoleMap()
 
 def target_agent_len(target,agent):
     return np.sqrt(np.square(target['x'] - agent.x) + np.square(target['y'] - agent.y))
-
 
 def walk(target,agent):
     x = 0
@@ -69,41 +30,53 @@ def walk(target,agent):
 
 def no_target_walk(area, agent, height, width):
     direction = [0, 0, 0, 0, 0]
-    for i in range(0, width):
+    target = [0, 0, 0, 0, 0]
+    
+    for i in range(0, width): # detect target and unseenspace in four direction for agent 
         for j in range(0, height):
             if i >= 0 and i < agent.x:
                 if j >= 0 and j < agent.y:
                     if area[i][j] == -3:
                         direction[2] += 1
+                    if area[i][j] == -2:
+                        target[2] += 1
                 elif j > agent.y and j < width:
                     if area[i][j] == -3:
                         direction[3] += 1
+                    if area[i][j] == -2:
+                        target[3] += 1
             elif i > agent.x and i < height:
                 if j >= 0 and j < agent.y:
                     if area[i][j] == -3:
                         direction[1] += 1
+                    if area[i][j] == -2:
+                        target[1] += 1
                 elif j > agent.y and j < width:
                     if area[i][j] == -3:
                         direction[4] += 1
+                    if area[i][j] == -2:
+                        target[4] += 1
      
     ind = -1    
     maximum = 0
-    for k in range(0, 5):
+    for k in range(0, len(direction)):
         if ind == -1:
             maximum = direction[0]
             ind = 0
         elif direction[k] > maximum:
             maximum = direction[k]
             ind = k
+        
     if ind == 0:
-        if agent.x <= np.int(width / 2) and agent.y <= np.int(height / 2):
-            ind = 4
-        elif agent.x <= np.int(width / 2) and agent.y > np.int(height / 2):
-            ind = 1
-        elif agent.x > np.int(width / 2) and agent.y <= np.int(height / 2):
-            ind = 3
-        elif agent.x > np.int(width / 2) and agent.y > np.int(height / 2):
-            ind = 2
+        maximum = 0
+        ind == -1
+        for k in range(0, len(target)):
+            if ind == -1:
+                maximum = target[0]
+                ind = 0
+            elif target[k] > maximum:
+                maximum = target[k]
+                ind = k
     return ind
 
 def haveunseenspace(area, height, width):
@@ -129,7 +102,6 @@ def testManualGameImageOutput2():
     game = Game(height, width)
 
     game.setRandomMap(0, 200, 0) # numbers of agents, targets, obstacles
-    #game.setScore(100, 10, -0.01, -100)
 
 
     game.printGodMap()
@@ -162,7 +134,7 @@ def testManualGameImageOutput2():
             belongs[index].append(item)
         
         cmd = [] # store the new command for agents 
-
+        
         for i in agents:
             
             if mode[i] == False: # assign a target to agent 
@@ -175,17 +147,17 @@ def testManualGameImageOutput2():
                         if target_agent_len(target_list,agents[i]) < target_agent_len(now_target[i],agents[i]):
                             now_target[i] = target_list
             
-            no_target_command = { 1: {"dx": 1, "dy": -1},
+            no_target_command = { 0: {"dx": 0, "dy": 0},
+                                  1: {"dx": 1, "dy": -1},
                                   2: {"dx": -1, "dy": -1},
-                                  3: {"dx": -1, "dy": -1},
-                                  4: {"dx": 1, "dy": -1},
+                                  3: {"dx": -1, "dy": 1},
+                                  4: {"dx": 1, "dy": 1},
                                  }
             
             if mode[i] == False: # assign the cammand to agent
                 direction = no_target_walk(game.getmap(), agents[i], height, width)
-                if direction > 0:
-                    cmd.append(Command(agents[i].id, no_target_command[direction]["dx"],no_target_command[direction]["dy"]))
-                    print("agent %d goes" % agents[i].id,no_target_command[direction]["dx"], " and ", no_target_command[direction]["dy"])
+                cmd.append(Command(agents[i].id, no_target_command[direction]["dx"],no_target_command[direction]["dy"]))
+                print("agent %d goes" % agents[i].id,no_target_command[direction]["dx"], "and", no_target_command[direction]["dy"])
             elif mode[i] == True:
                 cmd.append(walk(now_target[i],agents[i]))
                 mode[i] = False
